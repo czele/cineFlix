477ab455cce3855fb425f165d3bc7d0973b3474f
Camila Zelenovsky <camila_zele@hotmail.com>
2024-07-31 15:31:15 -0300

    criação de pastas e roteamento funcionando

--- cineFlix/src/app/app-routing.module.ts
+++ cineFlix/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CollectionComponent } from './collection/collection.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'collection', component: CollectionComponent },
+  { path: 'favorite', component: FavoriteComponent },
+  { path: '', redirectTo: '/collection', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cineFlix/src/app/app.module.ts
+++ cineFlix/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionComponent } from './collection/collection.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
