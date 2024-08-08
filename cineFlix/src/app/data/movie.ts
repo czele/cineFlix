028e67b1b9b74069316d82e16773b7bf719d0f42
Camila Zelenovsky <camila_zele@hotmail.com>
2024-08-07 23:20:11 -0300

    objeto filme criado

---
+++ cineFlix/src/app/data/movie.ts
@@ -0,0 +1,13 @@
+export interface Movie {
+    title?:string;
+    year?:string;
+    released?:string;
+    runtime?:string;
+    genre?:string;
+    director?:string;
+    actors?:string;
+    plot?:string;
+    language?:string;
+    country?:string;
+    poster?:string;
+}
